b4014f9322b71a10d224e67ea58ad228a8b0eb33
mtrxgamer <mtrxgamer@gmail.com>
2026-07-01 15:58:29 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,3 +1,7 @@
 import streamlit as st
+
 st.title('El mejor Pokemon')
-st.image('https://www.wikidex.net/wiki/Pok%C3%A9mon_Esmeralda')
+
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQepGGijcbZSBi1FpzNoTmlVK9rudUMZ2dMHj80EPxyPQ&s=10')
+
+st.write('made by mtrxgamer')
