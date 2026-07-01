f9967c3c45dd8879c4b0a04dbf53a72a62add8c8
mtrxgamer <mtrxgamer@gmail.com>
2026-07-01 16:11:55 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('El mejor Pokemon')
 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQepGGijcbZSBi1FpzNoTmlVK9rudUMZ2dMHj80EPxyPQ&s=10')
 
 st.write('made by mtrxgamer')
+
+df = pd.read_csv('https://raw.githubusercontent.com/mtrxgamer/proyecto_uno/refs/heads/main/pokemon.csv')
+
+st.write(df)
